34c43acc98365a9e1684e5eb4536cc9c02e78a75
Jochen Lillich <jochen@lillich.info>
2015-05-17 16:27:53 +0100

    Improved install script

--- install.sh
+++ install.sh
@@ -8,14 +8,22 @@ cd ~
 #
 
 # oh-my-zsh
-if [[ ! -d .oh-my-zsh ]]; then
-  git clone https://github.com/geewiz/oh-my-zsh.git .oh-my-zsh
+OMZDIR=".oh-my-zsh"
+if [[ -d $OMZDIR ]]; then
+  echo "Creating backup of existing oh-my-zsh"
+  BACKUPDIR=.oh-my-zsh.bak
+  test -d $BACKUPDIR && rm -rf $BACKUPDIR
+  mv $OMZDIR $BACKUPDIR
+  mv .zshrc .zshrc.bak
 fi
+echo "Installing oh-my-zsh"
+curl -L https://raw.github.com/geewiz/oh-my-zsh/master/tools/install.sh | sh
 
 #
 # Symlink dotfiles
 #
 
+echo "Creating symlinks"
 for df in $dfdir/_*; do
     name=$(basename $df)
     ln -sf $df .${name#_*}
