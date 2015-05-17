03f654822367e1240b4db9e893857557213965c6
Jochen Lillich <jochen@lillich.info>
2015-05-17 16:16:49 +0100

    Added installation of oh-my-zsh

--- install.sh
+++ install.sh
@@ -3,6 +3,19 @@
 dfdir=$(pwd)
 cd ~
 
+#
+# Install prerequisites
+#
+
+# oh-my-zsh
+if [[ ! -d .oh-my-zsh ]]; then
+  git clone https://github.com/geewiz/oh-my-zsh.git .oh-my-zsh
+fi
+
+#
+# Symlink dotfiles
+#
+
 for df in $dfdir/_*; do
     name=$(basename $df)
     ln -sf $df .${name#_*}
