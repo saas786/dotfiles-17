e163e5be81db3fa60ae44034bd7dd001ac8ec6d0
Jochen Lillich <contact@geewiz.dev>
2020-08-03 17:17:38 +0100

    zsh: remove Pharos, add HCloud

---
+++ zsh/hcloud.zsh
@@ -0,0 +1,5 @@
+# Hetzner Cloud CLI
+
+if which hcloud >/dev/null; then
+  source <(hcloud completion zsh)
+fi
--- zsh/pharos.zsh
+++
@@ -1,4 +0,0 @@
-CHPHAROS=/usr/local/opt/chpharos/share/chpharos/chpharos.sh
-if [[ -f $CHPHAROS ]]; then
-  source $CHPHAROS
-fi
