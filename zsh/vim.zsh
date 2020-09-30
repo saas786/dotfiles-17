35498381980d8f72c6a1154f7d9e6a21a3ee0350
Jochen Lillich <contact@geewiz.dev>
2020-09-30 09:54:25 +0100

    Revert zsh to Emacs mode

--- zsh/vim.zsh
+++ zsh/vim.zsh
@@ -1,14 +1,5 @@
-# vim mode config
-# ---------------
-
-# Activate vim mode.
-bindkey -v
-
-# Remove mode switching delay.
-KEYTIMEOUT=5
-
-# Preserve search hotkey
-bindkey '^R' history-incremental-search-backward
+# vim settings for shell
+# ----------------------
 
 which nvim >/dev/null && alias vim=nvim
 
