c392813587cd87755b2cb4a88e43cc7bb3e968d2
Jochen Lillich <contact@geewiz.dev>
2020-10-05 15:54:51 +0100

    Enable hist_ignore_space for zsh

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -1,5 +1,5 @@
 # history settings
-setopt hist_ignore_all_dups inc_append_history
+setopt hist_ignore_all_dups inc_append_history hist_ignore_space
 HISTFILE=~/.zhistory
 HISTSIZE=4096
 SAVEHIST=4096
